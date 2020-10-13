6fc52a015e59a063cc8323ee1acee12817130a89
hoang man <hoangman772@gmail.com>
2020-10-13 15:55:11 +0700

    add mongoose server

--- src/database/util.ts
+++ src/database/util.ts
@@ -10,6 +10,14 @@ const Util = {
     port: process.env.MONGODB_PORT || '27017',
     db: process.env.MONGODB_DB || 'graphql',
   },
+  mongoServer: {
+    host: process.env.MONGODB_HOST_SERVER || 'cluster0.ascy6.mongodb.net',
+    user: process.env.MONGODB_USER_SERVER || 'hoangman',
+    pass: process.env.MONGODB_PASS_SERVER || '123',
+    db: process.env.MONGODB_DB_SERVER || 'graphql',
+    auth: process.env.MONGODB_SERVER
+    || 'authSource=admin&replicaSet=Cluster0-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true',
+  },
 };
 
 if (!Util.mongo.host) {
