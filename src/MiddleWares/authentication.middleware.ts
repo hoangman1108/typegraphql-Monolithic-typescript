2229e45e0b0fb95cfbcc924593c4c53a35442a2f
hoangman <hoangman772@gmail.com>
2020-10-28 07:47:46 +0700

    update

--- src/MiddleWares/authentication.middleware.ts
+++ src/MiddleWares/authentication.middleware.ts
@@ -2,8 +2,9 @@ import { MiddlewareInterface, ResolverData, NextFn } from 'type-graphql';
 import { isFunction, isEmpty } from 'lodash';
 import { IContext } from '../App/Context';
 
-import AuthService from '../services/auth.service';
-import authHelper from '../Utils/auth';
+// import AuthService from '../services/auth.service';
+// import authHelper from '../Utils/auth';
+
 export class AuthenticationMiddleware implements MiddlewareInterface<IContext> {
   async use({ info, context }: ResolverData<IContext>, next: NextFn) {
     const { authenticate } = info?.parentType.getFields()[info.fieldName].extensions || {};
@@ -12,14 +13,13 @@ export class AuthenticationMiddleware implements MiddlewareInterface<IContext> {
       context.user = { ...user };
     } else if (authenticate === true) {
       const jwt = await context.authenticate('jwt', { session: false }, context.req, context.res);
-      if (jwt.info) {
-        const authService = new AuthService();
-        const refreshToken = await authService.getRefreshToken(context.req.headers?.authorization || '');
-        const verifyToken = await authHelper.verifyToken(refreshToken);
-        console.log(verifyToken);
-        const error = new Error(jwt.info);
-        throw error;
-      }
+      // if (jwt.info) {
+      //   const authService = new AuthService();
+      //   const refreshToken = await authService.getRefreshToken(context.req.headers?.authorization || '');
+      //   const verifyToken = await authHelper.verifyToken(refreshToken);
+      //   const error = new Error(jwt.info);
+      //   throw error;
+      // }
       if (!isEmpty(jwt.user)) {
         context.user = jwt.user;
       } else {
